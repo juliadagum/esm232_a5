da11a6b9e416b665920585213f74b9e0a43aaec2
juliadagum <jdagum@ucsb.edu>
2021-04-28 09:18:47 -0700

    Started assignment in class

---
+++ dpopk.R
@@ -0,0 +1,14 @@
+
+#'  Simple population growth
+#' @param time time
+#' @param P initial population
+#' @param r intrinsic growth rate 
+#' @param K carrying capacity
+#' @return derivative of population with time 
+#' @examples use with ode solver
+#' ode(y=1,time=c(1;100),dexppop, parms=c(0.012))
+
+dpopk = function(time, P, r, K) {
+  dpopk = r*P*(1-P/K)
+  return(list(dpopk))
+}
